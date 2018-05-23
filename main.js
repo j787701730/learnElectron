63dca47e3220260a38c9959b0f5682ecf6cf546c
zhengw <247276359@qq.com>
2018-05-23 15:54:41 +0800

    修改

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    resizable: false,
+    maximizable: false
   })
 
   // and load the index.html of the app.
